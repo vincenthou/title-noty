9069b2ac487b4157c04d23e3a21892284e690b82
vincenthou <vincenthou@augmentum.com.cn>
2015-01-13 18:28:02 +0800

    [Feature] Initial version

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # titleNoty
-simple plugin to notify information by refreshing title name
+Simple plugin to notify information by refreshing title name, cancel message notifing when current page is visiable.
+
+#Usage
+There are three types supported now: flash, scroll, addition.
+
+* Flash effect
+
+This is the default effect, so the effect option is needless.
+The message will show and hide periodically based on **checkInterval** configuration.
+
+```javascript
+titleNoty.init({
+    message: 'You have unread message'
+    effect: 'flash'
+});
+```
+
+* Scroll effect
+
+The message will scroll from right to the left periodically based on **checkInterval** configuration.
+
+```javascript
+titleNoty.init({
+    message: 'You have unread message'
+    effect: 'flash'
+});
+```
+
+* Adition effect
+
+The title will be kept and extra count number is added before title.
+**counterWrapper** is used to specify the wrapper for counter, default wrapper is '(count)', count is used as the placeholder for replacing the count number.
+
+```javascript
+titleNoty.init({
+    effect: 'addition'
+});
+```
+
+Add count number
+```javascript
+titleNoty.add();
+```
+
+Minus count number
+```javascript
+titleNoty.minus();
+```
+
+Set count number directly, the number should be number or string containing number.
+```javascript
+titleNoty.set(10);
+```
+
+# TODO
+* Support notification
+* Test Browser compability
+* Bower support
+* Build tasks
---
+++ js/titleNoty.js
@@ -0,0 +1,65 @@
+(function(win, undef){
+    var checkInterval = 100;
+    titleNoty = {
+        init: function (config) {
+            this.checkInterval = config.checkInterval || 200;
+            this.effect = config.effect || 'flash'
+            this.title = config.title || document.title;
+            this.message = config.message || this.title;
+            this.currentTitle = this.message;
+            if (this.effect !== 'addition') {
+                this.timer = setInterval(this.render, this.checkInterval);
+            } else {
+                this.counter = 0;
+                this.counterWrapper = config.counterWrapper || '(count)';
+            }
+            var self = this;
+            document.addEventListener("visibilitychange", function(evt) {
+                if (!document.hidden) {
+                    clearInterval(self.timer);
+                    self.timer = null;
+                    self.currentTitle = self.title;
+                }
+            }, false);
+            return this;
+        },
+        render: function() {
+            self = titleNoty;
+            document.title = self.currentTitle;
+            switch (self.effect) {
+                case 'flash': {
+                    self.currentTitle = (self.title === self.currentTitle) ? self.message : self.title;
+                }
+                break;
+                case 'scroll': {
+                    self.currentTitle = self.currentTitle.slice(1)
+                    !self.currentTitle.length && (self.currentTitle = self.message);
+                }
+                break;
+            }
+        },
+        add: function() {
+            this.counter++;
+            this.replaceCount();
+        },
+        minus: function() {
+            this.counter--;
+            (this.counter < 0) && (this.counter = 0);
+            this.replaceCount();
+        },
+        set: function(number) {
+            number = parseInt(number);
+            if (!isNaN(number)) {
+                this.counter = number;
+                (this.counter < 0) && (this.counter = 0);
+                this.replaceCount();
+            }
+        }
+        replaceCount: function() {
+            counterStr = this.counterWrapper.replace('count', this.counter)
+            !this.counter && (counterStr = '')
+            document.title = counterStr + this.title;
+        }
+    }
+    win.titleNoty = titleNoty;
+}(window, undefined))
