38a2f392a6106885f3af2f79691d213a8169fe2e
vincenthou <vincenthou365@gmail.com>
2015-09-16 23:45:49 +0800

    [Feature] Update doc, add editorconfig and new examples, refine code for new API design, include src for bower

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Title noty
+# Title titleNoty
 
-Simple plugin to notify information by refreshing title name, cancel message notifing when current page is visiable. Vanilla javascript with no dependency.
+Simple plugin to notify information by refreshing title name, cancel message notifing when current page is visiable. Vanilla javascript with no dependency, **less than 3kb** after minified.
 
 #Installation
 
@@ -12,7 +12,7 @@ bower install title-noty
 
 #Usage
 
-There are three types supported now: flash, scroll, addition.
+There are three types supported now: flash, scroll, add and subtract number.
 
 ## Show title message with flash effect
 
@@ -20,7 +20,8 @@ This is the default effect, so the effect option is needless.
 The message will show and hide periodically based on **checkInterval** configuration.
 
 ```javascript
-noty.flash('You have unread message');
+titleNoty.flash(); //Flash current title
+titleNoty.flash('You have unread message'); //Flash between current title and specified message
 ```
 
 ## Show title message with scroll effect
@@ -28,7 +29,8 @@ noty.flash('You have unread message');
 The message will scroll from right to the left periodically based on **checkInterval** configuration.
 
 ```javascript
-noty.scroll('You have unread message');
+titleNoty.scroll(); //Scroll current title
+titleNoty.scroll('You have unread message'); //Scroll specified message
 ```
 
 ## Add and subtract count number with plain text
@@ -38,41 +40,41 @@ The original title will be kept and extra count number is added before title.
 Add count number
 
 ```javascript
-noty.add();
+titleNoty.add();
 ```
 
 Subtract count number
 
 ```javascript
-noty.sub();
+titleNoty.sub();
 ```
 
 Set count number directly, the number should be number or string containing number.
 
 ```javascript
-noty.set(10);
+titleNoty.set(10);
 ```
 
 ## Add and subtract count number on favicon image
 
-All the count number manipulation methods are the same as examples above. In addition, you should tell `noty` you want to add number with favicon
+All the count number manipulation methods are the same as examples above. In addition, you should tell `titleNoty` you want to add number with favicon
 
 ```javascript
-noty.favicon();
+titleNoty.favicon();
 ```
 
 If you want to draw the number at the bottom right of original favicon image, pass a true value to the method.
 
 ```javascript
-noty.favicon(true);
+titleNoty.favicon(true);
 ```
 
 ## Advanced configuration and usage
 
-Advanced configuration can be specified with init method of `noty` before calling other methods.
+Advanced configuration can be specified with init method of `titleNoty` before calling other methods.
 
 ```javascript
-noty.init({
+titleNoty.init({
     numberWrapper: '[number]',
     message: 'You have unread message',
     interval: 100,
@@ -84,10 +86,16 @@ noty.init({
 
 * **numberWrapper** is used to specify the wrapper for number, default wrapper is '(number)', `number` is used as the placeholder for replacing the count number.
 * **message** is used to specify the messages to be shown with flash and scroll effect. If specified with `init` method, you can call `flash` and `scroll` directly.
-* **interval** is used to specify the time interval for flash and scroll effect. If not specified, default value is **200**.
+* **interval** is used to specify the time interval for flash and scroll effect. If not specified, default value is **500**.
 * **title** is used to specify the initial title to be shown. If not specified, default value is current title value.
 * **number** is used to specify the initial number to count. If not specified, default value is **0**.
 * **max** is used to specify the maximum count number, after the number is reached, a plus symbol will be added. If not specified, default value is **99**, count number exceed 99 will be displayed as `99+`.
+
+Current number can be got directly
+
+```javascript
+console.log(titleNoty.number);
+```
 
 # TODO
 * Support notification
--- js/titleNoty.js
+++ js/titleNoty.js
@@ -1,18 +1,22 @@
 (function(win, undef){
-    titleNoty = {
+    noty = {
         init: function (config) {
             !config && (config = {});
-            this.interval = config.interval || 200;
+            var self = this;
+            this.interval = config.interval || 500;
             this.title = config.title || document.title;
             this.message = config.message || this.title;
+            this.numberWrapper = config.numberWrapper || '(number)';
+            this.max = config.max || 99;
+            this.effect = config.effect || '';
             this.currentTitle = this.message;
-            if (this.interval) {
-                this.timer = setInterval(this._render, this.interval);
-            } else {
-                this.number = 0;
-                this.numberWrapper = config.numberWrapper || '(number)';
+            this.number = 0;
+            if (this.effect) {
+                this.timer = setInterval(function(){
+                    document.title = self.currentTitle;
+                    noty['_' + self.effect].call(self);
+                }, this.interval);
             }
-            var self = this;
             document.addEventListener("visibilitychange", function(evt) {
                 if (!document.hidden) {
                     clearInterval(self.timer);
@@ -22,53 +26,68 @@
             }, false);
             return this;
         },
-        _render: function() {
-            self = this;
-            document.title = self.currentTitle;
-            switch (self.effect) {
-                case 'flash': {
-                    self.currentTitle = (self.title === self.currentTitle) ? self.message : self.title;
-                }
-                break;
-                case 'scroll': {
-                    self.currentTitle = self.currentTitle.slice(1)
-                    !self.currentTitle.length && (self.currentTitle = self.message);
-                }
-                break;
-            }
+        _flash: function() {
+            (this.title === this.message) && (this.message = '');
+            this.currentTitle = (this.title === this.currentTitle) ? this.message : this.title;
+        },
+        _scroll: function() {
+            this.currentTitle = this.currentTitle.slice(1)
+            !this.currentTitle.length && (this.currentTitle = this.message);
         },
         add: function() {
             !this.interval && this.init();
             this.number++;
-            this._replaceNum();
+            noty._replaceNum.call(this);
         },
         sub: function() {
             !this.interval && this.init();
             this.number--;
             (this.number < 0) && (this.number = 0);
-            this._replaceNum();
+            noty._replaceNum.call(this);
         },
         set: function(number) {
             !this.interval && this.init();
-            number = parseInt(number);
+            var number = parseInt(number);
             if (!isNaN(number)) {
                 this.number = number;
                 (this.number < 0) && (this.number = 0);
-                this._replaceNum();
+                noty._replaceNum.call(this);
             }
         },
         _replaceNum: function() {
-            numberStr = this.numberWrapper.replace('count', this.number)
+            var number = this.number > this.max ? (this.max + '+') : this.number;
+            var numberStr = this.numberWrapper.replace('number', number)
             !this.number && (numberStr = '')
             document.title = numberStr + this.title;
+        },
+        favicon: function() {
+            console.warn("Not implemented yet");
         }
     };
+    //Define methods exposed
+    var exposed = {
+        init: noty.init,
+        add: noty.add,
+        sub: noty.sub,
+        set: noty.set,
+        favicon: noty.favicon
+    };
+    //Add effect methods
+    var effects = ['flash', 'scroll'];
+    for (var i = 0; i < effects.length; i++) {
+        var effect = effects[i]
+        exposed[effect] = (function(effect) {
+            return function(msg) {
+                !this.interval && this.init({message:msg, effect:effect});
+            };
+        })(effect);
+    }
     //Support AMD module definition
     if (typeof define === "function" && define.amd) {
         define( "titleNoty", [], function() {
-            return titleNoty;
+            return exposed;
         });
     } else {
-        win.titleNoty = titleNoty;
+        win.titleNoty = exposed;
     }
 }(window, undefined));
